3dfcbead4bb990a31890722c661120d2083c86c1
imeldaloei <imeldaloei@gmail.com>
2018-06-08 15:47:59 -0700

    Update test-connect-to-board.js

--- trello-webhooks/test-connect-to-board.js
+++ trello-webhooks/test-connect-to-board.js
@@ -8,6 +8,6 @@ xhr.addEventListener("readystatechange", function () {
   }
 });
 
-xhr.open("GET", "https://api.trello.com/1/boards/5ae78bd40518a0ccfdd7026a?key=4acd1a952c7f20dfeaa556f8f7236a5f&token=440c72ede07ae4b3b61ba3c2567baf39189ae15359dcf9700c49615c24589f57");
+xhr.open("POST", "https://api.trello.com/1/cards/5ae78c97c0be43b6f52e1596/idMembers?value=5a7505a37800d36bcec85828&key=4acd1a952c7f20dfeaa556f8f7236a5f&token=440c72ede07ae4b3b61ba3c2567baf39189ae15359dcf9700c49615c24589f57");
 
 xhr.send(data);
