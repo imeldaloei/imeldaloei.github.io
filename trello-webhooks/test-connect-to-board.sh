19427082fe71d8dc2b39b45c9c28a38d83b78cc5
imeldaloei <imelda.loei@nbcuni.com>
2018-06-08 15:22:47 -0700

    Update test-connect-to-board.sh

--- trello-webhooks/test-connect-to-board.sh
+++ trello-webhooks/test-connect-to-board.sh
@@ -1,6 +1,4 @@
 # {"id":"5b1af4484ed47b7954f1ef07","description":"Test Connect Board","idModel":"5ae78bd40518a0ccfdd7026a","callbackURL":"https://imeldaloei.github.io/trello-webhooks/test-connect-to-board.sh","active":true}
 
-response=`curl --request GET \
-  --url 'https://api.trello.com/1/boards/5ae78bd40518a0ccfdd7026a?key=4acd1a952c7f20dfeaa556f8f7236a5f&token=440c72ede07ae4b3b61ba3c2567baf39189ae15359dcf9700c49615c24589f57'`
-
-echo $response > ./response.txt
+curl --request POST \
+  --url 'https://api.trello.com/1/cards/5ae78c97c0be43b6f52e1596/labels?color=null&name=test&key=4acd1a952c7f20dfeaa556f8f7236a5f&token=440c72ede07ae4b3b61ba3c2567baf39189ae15359dcf9700c49615c24589f57'
