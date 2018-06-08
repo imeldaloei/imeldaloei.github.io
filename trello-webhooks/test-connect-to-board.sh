9668178e3eedb050e4c6bf33793430193c75b8cd
imeldaloei <imeldaloei@gmail.com>
2018-06-08 15:32:37 -0700

    Update test-connect-to-board.sh

--- trello-webhooks/test-connect-to-board.sh
+++ trello-webhooks/test-connect-to-board.sh
@@ -3,5 +3,5 @@
 curl --request POST \
   --url 'https://api.trello.com/1/cards/5ae78c97c0be43b6f52e1596/labels?color=null&name=test&key=4acd1a952c7f20dfeaa556f8f7236a5f&token=440c72ede07ae4b3b61ba3c2567baf39189ae15359dcf9700c49615c24589f57'
   
-curl --request POST \
-  --url 'https://api.trello.com/1/cards/5ae78c97c0be43b6f52e1596/idMembers?value=5a7505a37800d36bcec85828&key=4acd1a952c7f20dfeaa556f8f7236a5f&token=440c72ede07ae4b3b61ba3c2567baf39189ae15359dcf9700c49615c24589f57'
+# curl --request POST \
+#  --url 'https://api.trello.com/1/cards/5ae78c97c0be43b6f52e1596/idMembers?value=5a7505a37800d36bcec85828&key=4acd1a952c7f20dfeaa556f8f7236a5f&token=440c72ede07ae4b3b61ba3c2567baf39189ae15359dcf9700c49615c24589f57'
