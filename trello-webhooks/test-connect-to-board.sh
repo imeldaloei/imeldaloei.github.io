5ed5d66a3a4917e33204c649917207b10b89fe4f
imeldaloei <imelda.loei@nbcuni.com>
2018-06-08 14:22:49 -0700

    Create test-connect-to-board.sh

---
+++ trello-webhooks/test-connect-to-board.sh
@@ -0,0 +1,2 @@
+curl --request GET \
+  --url 'https://api.trello.com/1/boards/5ae78bd40518a0ccfdd7026a?key=4acd1a952c7f20dfeaa556f8f7236a5f&token=440c72ede07ae4b3b61ba3c2567baf39189ae15359dcf9700c49615c24589f57'
